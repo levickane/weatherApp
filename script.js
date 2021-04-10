36ff62c96d1bf87480405815f443d2d7751f4809
Levi Kane <oneleggedlevi09@gmail.com>
2021-04-10 15:56:58 -0500

    have most of the layout done. need to work on retreiving the correct data from the api

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log('working')
+requestUrl =
+  //   'https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=b47cbc5dede4579f5550da5d2bb84dc3'
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (data) {
+      console.log('Fetch Response \n-------------')
+      console.log(data)
+    })
