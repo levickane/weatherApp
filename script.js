ab40d2129d7c348e7cc97786bb1ebcdff16c563c
Levi Kane <oneleggedlevi09@gmail.com>
2021-04-11 21:50:33 -0500

    all icons are rendering and the search history is linked to local storage and rendering properly. when clicking on the history, it loads the city info

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var currentDate = $('.currentDate')
+var calDate = moment().format('L')
+currentDate.append('(' + calDate + ')')
+
 function getMainWeather(cityName) {
   fetch(`  https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=b47cbc5dede4579f5550da5d2bb84dc3
   `)
@@ -5,7 +9,7 @@ function getMainWeather(cityName) {
       return response.json()
     })
     .then(function (data) {
-      console.log(data)
+      //   console.log(data)
       var featuredCityName = $('.featuredCityName')
       var featuredTemp = $('.featuredTemp')
       var featuredHumidity = $('.featuredHumidity')
@@ -59,19 +63,6 @@ function getMainWeather(cityName) {
     })
 }
 
-var searchButton = $('#searchButton')
-var clearSearch = $('#searchBar')
-searchButton.on('click', () => {
-  var searchBar = $('#searchBar').val()
-  getMainWeather(searchBar)
-  get5DayForecast(searchBar)
-  clearSearch.val('')
-})
-
-var currentDate = $('.currentDate')
-var calDate = moment().format('L')
-currentDate.append('(' + calDate + ')')
-
 function get5DayForecast(cityName) {
   fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=b47cbc5dede4579f5550da5d2bb84dc3`
@@ -104,66 +95,99 @@ function get5DayForecast(cityName) {
         var humidity = data.list[i].main.humidity
         fiveDayHumidityArray.push(humidity)
         var weatherIconImg = data.list[i].weather[0].icon
+        console.log(weatherIconImg)
         fiveDayWeatherIconArray.push(weatherIconImg)
       }
       for (var i = 0; i < fiveDayDateArray.length; i++) {
         fiveDayDate[i].textContent = fiveDayDateArray[i]
         fiveDayTemp[i].textContent = fiveDayTempArray[i]
         fiveDayHumidity[i].textContent = fiveDayHumidityArray[i]
-        // fiveDayWeatherIcon[i].textContent = fiveDayWeatherIconArray[i]
 
-        var sunny = $('<i class="bi bi-brightness-high"></i>')
-        var moon = $('<i class="bi bi-moon-stars"></i>')
-        var sunnyWithCloud = $('<i class="bi bi-cloud-sun"></i>')
-        var cloudy = $('<i class="bi bi-cloud-fill"></i>')
-        var heavyClouds = $('<i class="bi bi-clouds-fill"></i>')
-        var foggy = $('<i class="bi bi-cloud-haze"></i>')
-        var snow = $('<i class="bi bi-snow2"></i>')
-        var rain = $('<i class="bi bi-cloud-rain"></i>')
-        var thunder = $('<i class="bi bi-cloud-lightning-rain-fill"></i>')
-
+        var sunny = $('<i class="bi bi-brightness-high"></i>')[0]
+        var moon = $('<i class="bi bi-moon-stars"></i>')[0]
+        var sunnyWithCloud = $('<i class="bi bi-cloud-sun"></i>')[0]
+        var cloudy = $('<i class="bi bi-cloud-fill"></i>')[0]
+        var heavyClouds = $('<i class="bi bi-clouds-fill"></i>')[0]
+        var foggy = $('<i class="bi bi-cloud-haze"></i>')[0]
+        var snow = $('<i class="bi bi-snow2"></i>')[0]
+        var rain = $('<i class="bi bi-cloud-rain"></i>')[0]
+        var thunder = $('<i class="bi bi-cloud-lightning-rain-fill"></i>')[0]
+        console.log(fiveDayList[i])
         if (fiveDayWeatherIconArray[i] == '01d') {
-          fiveDayWeatherIcon[i].textContent = sunny
+          fiveDayWeatherIcon[i].append(sunny)
         } else if (fiveDayWeatherIconArray[i] == '01n') {
-          fiveDayWeatherIcon[i].textContent = moon
+          fiveDayWeatherIcon[i].append(moon)
         } else if (
           fiveDayWeatherIconArray[i] == '02n' ||
           fiveDayWeatherIconArray[i] == '02d'
         ) {
-          fiveDayWeatherIcon[i].textContent = sunnyWithCloud
+          fiveDayWeatherIcon[i].append(sunnyWithCloud)
         } else if (
           fiveDayWeatherIconArray[i] == '03n' ||
           fiveDayWeatherIconArray[i] == '03d'
         ) {
-          fiveDayWeatherIcon[i].textContent = cloudy
+          fiveDayWeatherIcon[i].append(cloudy)
         } else if (
           fiveDayWeatherIconArray[i] == '04n' ||
           fiveDayWeatherIconArray[i] == '04d'
         ) {
-          fiveDayWeatherIcon[i].textContent = heavyClouds
+          fiveDayWeatherIcon[i].append(heavyClouds)
         } else if (
           fiveDayWeatherIconArray[i] == '50n' ||
           fiveDayWeatherIconArray[i] == '50d'
         ) {
-          fiveDayWeatherIcon[i].textContent = foggy
+          fiveDayWeatherIcon[i].append(foggy)
         } else if (
           fiveDayWeatherIconArray[i] == '13n' ||
           fiveDayWeatherIconArray[i] == '13d'
         ) {
-          fiveDayWeatherIcon[i].textContent = snow
+          fiveDayWeatherIcon[i].append(snow)
         } else if (
           fiveDayWeatherIconArray[i] == '09n' ||
-          fiveDayWeatherIconArray[i] == '09d'
+          fiveDayWeatherIconArray[i] == '09d' ||
+          fiveDayWeatherIconArray[i] == '10n' ||
+          fiveDayWeatherIconArray[i] == '10d'
         ) {
-          fiveDayWeatherIcon[i].textContent = rain
+          fiveDayWeatherIcon[i].append(rain)
         } else if (
           fiveDayWeatherIconArray[i] == '11n' ||
           fiveDayWeatherIconArray[i] == '11d'
         ) {
-          fiveDayWeatherIcon[i].textContent = thunder
+          fiveDayWeatherIcon[i].append(thunder)
+        } else {
+          console.log(`unknown ${fiveDayWeatherIconArray[i]}`)
         }
       }
     })
 }
 
-// get5DayForecast()
+var searchButton = $('#searchButton')
+var clearSearch = $('#searchBar')
+var historyUl = $('.historyUl')
+searchButton.on('click', () => {
+  var searchBar = $('#searchBar').val()
+
+  getMainWeather(searchBar)
+  get5DayForecast(searchBar)
+  const historyArray = JSON.parse(localStorage.getItem('searchHistory')) ?? []
+  historyArray.push(searchBar)
+  localStorage.setItem('searchHistory', JSON.stringify(historyArray))
+  setSearchHistoryLi(searchBar)
+  clearSearch.val('')
+})
+
+const historyArray = JSON.parse(localStorage.getItem('searchHistory')) ?? []
+for (let searchBar of historyArray) {
+  setSearchHistoryLi(searchBar)
+}
+
+function setSearchHistoryLi(searchBar) {
+  var newLi = $(`<li class="list-group-item historyLi">`)
+  console.log(searchBar)
+  newLi.html(searchBar)
+  historyUl.append(newLi)
+  newLi.on('click', () => {
+    getMainWeather(searchBar)
+    get5DayForecast(searchBar)
+  })
+}
